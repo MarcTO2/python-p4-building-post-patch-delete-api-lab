a02408fe56ab1e9ca84a0a186cb03b4c03bab4db
Absalom <mrclabsalom@gmail.com>
2023-10-17 11:02:09 +0300

    Completed

--- server/app.py
+++ server/app.py
@@ -2,7 +2,6 @@
 
 from flask import Flask, request, make_response, jsonify
 from flask_migrate import Migrate
-
 from models import db, Bakery, BakedGood
 
 app = Flask(__name__)
@@ -11,7 +10,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
-
 db.init_app(app)
 
 @app.route('/')
@@ -20,50 +18,65 @@ def home():
 
 @app.route('/bakeries')
 def bakeries():
-
     bakeries = Bakery.query.all()
     bakeries_serialized = [bakery.to_dict() for bakery in bakeries]
-
-    response = make_response(
-        bakeries_serialized,
-        200
-    )
+    response = make_response(jsonify(bakeries_serialized), 200)
     return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-
     bakery = Bakery.query.filter_by(id=id).first()
     bakery_serialized = bakery.to_dict()
-
-    response = make_response(
-        bakery_serialized,
-        200
-    )
+    response = make_response(jsonify(bakery_serialized), 200)
     return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
     baked_goods_by_price = BakedGood.query.order_by(BakedGood.price).all()
-    baked_goods_by_price_serialized = [
-        bg.to_dict() for bg in baked_goods_by_price
-    ]
-    
-    response = make_response(
-        baked_goods_by_price_serialized,
-        200
-    )
+    baked_goods_by_price_serialized = [bg.to_dict() for bg in baked_goods_by_price]
+    response = make_response(jsonify(baked_goods_by_price_serialized), 200)
     return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
     most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
     most_expensive_serialized = most_expensive.to_dict()
+    response = make_response(jsonify(most_expensive_serialized), 200)
+    return response
 
-    response = make_response(
-        most_expensive_serialized,
-        200
-    )
+@app.route('/baked_goods', methods=['POST'])
+def create_baked_good():
+    data = request.form
+    name = data.get('name')
+    price = data.get('price')
+    new_baked_good = BakedGood(name=name, price=price)
+    db.session.add(new_baked_good)
+    db.session.commit()
+    response = make_response(jsonify(new_baked_good.to_dict()), 201)
+    return response
+
+@app.route('/bakeries/<int:id>', methods=['PATCH'])
+def update_bakery(id):
+    bakery = Bakery.query.filter_by(id=id).first()
+    if bakery is None:
+        return jsonify({'error': 'Bakery not found'}), 404
+
+    data = request.form
+    new_name = data.get('name', bakery.name)
+    bakery.name = new_name
+    db.session.commit()
+    response = make_response(jsonify(bakery.to_dict()), 200)
+    return response
+
+@app.route('/baked_goods/<int:id>', methods=['DELETE'])
+def delete_baked_good(id):
+    baked_good = BakedGood.query.get(id)
+    if baked_good is None:
+        return jsonify({'error': 'Baked Good not found'}), 404
+
+    db.session.delete(baked_good)
+    db.session.commit()
+    response = make_response(jsonify({'message': 'Baked Good successfully deleted'}), 200)
     return response
 
 if __name__ == '__main__':
